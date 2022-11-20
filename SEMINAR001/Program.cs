8a0cb38899da980e16ddccb2209e9ccd3c566ffb
Kirill Denisevitch <denviko00@gmail.com>
2022-11-20 10:41:57 +0300

    sem001

---
+++ SEMINAR001/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Vvedite celoe chislo");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num*num;
+//Console.WriteLine("Квадрат введенного числа равен "+square);
+Console.WriteLine($"Kvadrat vedennogo chisla {square}");
+
+if (num>9 && num<100)   // проверка на двузначное число
+{
+    Console.WriteLine("vi vveli dvuhznachnoe chislo");
+}
+
+while (num<=25)  // вывод всех чисел от введенного до 25
+{
+    Console.Write($"{num} ");
+    num++;
+}
+Console.WriteLine();
