9116fa9a8c19aef399f315105be15fca7b4ae21d
Kirill Denisevitch <denviko00@gmail.com>
2022-11-20 12:06:31 +0300

    sem021

--- SEMINAR001/Program.cs
+++ SEMINAR001/Program.cs
@@ -1,17 +1,111 @@
-﻿Console.WriteLine("Vvedite celoe chislo");
-int num = Convert.ToInt32(Console.ReadLine());
-int square = num*num;
-//Console.WriteLine("Квадрат введенного числа равен "+square);
-Console.WriteLine($"Kvadrat vedennogo chisla {square}");
+﻿// Console.WriteLine("Vvedite celoe chislo");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int square = num*num;
+// //Console.WriteLine("Квадрат введенного числа равен "+square);
+// Console.WriteLine($"Kvadrat vedennogo chisla {square}");
 
-if (num>9 && num<100)   // проверка на двузначное число
-{
-    Console.WriteLine("vi vveli dvuhznachnoe chislo");
-}
+// if (num>9 && num<100)   // проверка на двузначное число
+// {
+//     Console.WriteLine("vi vveli dvuhznachnoe chislo");
+// }
 
-while (num<=25)  // вывод всех чисел от введенного до 25
-{
-    Console.Write($"{num} ");
-    num++;
-}
-Console.WriteLine();
+// while (num<=25)  // вывод всех чисел от введенного до 25
+// {
+//     Console.Write($"{num} ");
+//     num++;
+// }
+// Console.WriteLine();
+
+
+// Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int square = num2*num2;
+// if (square == num1)
+// {
+//     Console.WriteLine("Первое число является квадратом второго числа");
+// }
+// else
+// {
+//     Console.WriteLine("Первое число не является квадратом второго числа");
+// }
+
+
+// Console.WriteLine("Введите число от 1 до 7");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+//   switch(num)
+//             {
+//                 case 1:
+//                     Console.WriteLine("Понедельник");
+//                     break;
+//                 case 2:
+//                     Console.WriteLine("Вторник");
+//                     break;
+//                 case 3:
+//                     Console.WriteLine("Среда");
+//                     break;
+//                 case 4:
+//                     Console.WriteLine("Четверг");
+//                     break;
+//                 case 5:
+//                     Console.WriteLine("Пятница");
+//                     break;
+//                 case 6:
+//                     Console.WriteLine("Суббота");
+//                     break;   
+//                 case 7:
+//                     Console.WriteLine("Воскресенье");
+//                     break;          
+//                 default:
+//                     Console.WriteLine("Вы ввели некорректное число!");
+//                     break;         
+//             }
+
+
+ 
+
+
+
+// Console.Write("Enter the number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int count = -n;
+
+// while (count <+ n)
+// {
+//     Console.Write(count + " ");
+//     count++;
+// }
+
+
+
+
+// Console.Write("Введи число N: ");
+// int numberN = Convert.ToInt32(Console.ReadLine());
+// int numberA = -numberN;
+// while(numberA<=numberN)
+// {
+// Console.Write($"{numberA} ");
+// numberA ++;
+// }
+// Console.WriteLine();
+
+
+
+// Console.WriteLine("Введите число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int num = -n;
+// while (num <= n)
+// {
+//     Console.Write($"{num} ");
+//     num++;
+// }
+
+
+// Console.WriteLine("Введите trehznachnoe число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int div1 = num%10;
+// Console.Write($"{div1} ");
+
