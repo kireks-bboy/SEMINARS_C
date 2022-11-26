ac2296e4ece6c5593924493ff7e83e4eb9e08e4b
Kirill Denisevitch <denviko00@gmail.com>
2022-11-26 10:35:57 +0300

    ssem002

---
+++ SEMINAR002/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = new Random().Next(10,100);
+Console.WriteLine($"Sgenerirovannoe sluch chislo {num}");
+int x1 = num/10;
+int x2 = num % 10;
+if (x1>x2) Console.WriteLine($"Naivolshaya cifra etogo chisla eto {x1}");
+else if (x1<x2) Console.WriteLine($"Naibolshaya cifra etogo chisla eto {x2}");
+else Console.WriteLine("cifri odinakovie");
+
+for (int i = 0; i<=num; i=i+2) Console.Write($"{i} ");
