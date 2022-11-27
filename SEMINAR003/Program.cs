2af9a339e8aa850b7eda23514e30905a00840626
Kirill Denisevitch <denviko00@gmail.com>
2022-11-27 10:37:45 +0300

    void_string3

--- SEMINAR003/Program.cs
+++ SEMINAR003/Program.cs
@@ -1,26 +1,37 @@
 ﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
 // причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
-
-void checkKoord(int x, int y)
+void CheckKoord(int x, int y)
 {
-  if (x > 0 && y > 0) System.Console.WriteLine("Eto chetvert' #1");
-  else if (x > 0 && y < 0) System.Console.WriteLine("Eto chetvert' #2");
-  else if (x < 0 && y > 0) System.Console.WriteLine("Eto chetvert' #3");
-  else if (x < 0 && y < 0) System.Console.WriteLine("Eto chetvert' #4");
-  else System.Console.WriteLine("Tochka nahoditsya na koordinatnoi osi");
+    if (x > 0 && y > 0) Console.WriteLine("eto chetvert' # 1");
+    else if (x > 0 && y < 0) Console.WriteLine("eto chetvert' # 4");
+    else if (x < 0 && y > 0) Console.WriteLine("eto chetvert' # 2");
+    else if (x < 0 && y < 0) Console.WriteLine("eto chetvert' # 3");
+    else Console.WriteLine("точка находится на координатной оси");
+}
+
+string CheckKoord2(int x, int y)
+{
+    string res;
+    if (x > 0 && y > 0) res = "это четверть № 1";
+    else if (x > 0 && y < 0) res = "это четверть № 4";
+    else if (x < 0 && y > 0) res = "это четверть № 2";
+    else if (x < 0 && y < 0) res = "это четверть № 3";
+    else res = "точка находится на координатной оси";
+    return res;
 }
 
 try
 {
-  System.Console.WriteLine("Vvedite koordinaty X");
-  int x = Convert.ToInt32(Console.ReadLine());
-  System.Console.WriteLine("Vvedite koordinaty Y");
-  int y = Convert.ToInt32(Console.ReadLine());
-  checkKoord(x, y);
+    Console.WriteLine("Введите координату Х");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координату Y");
+    int y = Convert.ToInt32(Console.ReadLine());
+    CheckKoord(x, y);
+    Console.WriteLine(CheckKoord2(x,y));
 }
 catch
 {
-  System.Console.WriteLine("Nado bilo vvodit' celie chisla!");
+    Console.WriteLine("Надо было вводить именно целые числа!");
 }
 
