3d048ef5a212f1e9acdd188f9e2fe9a8ef70da18
Kirill Denisevitch <denviko00@gmail.com>
2022-11-27 11:52:25 +0300

    zadacha2

--- SEMINAR003/Program.cs
+++ SEMINAR003/Program.cs
@@ -1,37 +1,49 @@
-﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
-// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+﻿// // Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// // причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
-void CheckKoord(int x, int y)
-{
-    if (x > 0 && y > 0) Console.WriteLine("eto chetvert' # 1");
-    else if (x > 0 && y < 0) Console.WriteLine("eto chetvert' # 4");
-    else if (x < 0 && y > 0) Console.WriteLine("eto chetvert' # 2");
-    else if (x < 0 && y < 0) Console.WriteLine("eto chetvert' # 3");
-    else Console.WriteLine("tochka nahodistsa na koordinatnoy osi");
-}
+// void CheckKoord(int x, int y)
+// {
+//     if (x > 0 && y > 0) Console.WriteLine("eto chetvert' # 1");
+//     else if (x > 0 && y < 0) Console.WriteLine("eto chetvert' # 4");
+//     else if (x < 0 && y > 0) Console.WriteLine("eto chetvert' # 2");
+//     else if (x < 0 && y < 0) Console.WriteLine("eto chetvert' # 3");
+//     else Console.WriteLine("tochka nahodistsa na koordinatnoy osi");
+// }
 
-string CheckKoord2(int x, int y)
-{
-    string res;
-    if (x > 0 && y > 0) res = "eto chetvert' # 1";
-    else if (x > 0 && y < 0) res = "eto chetvert' # 2";
-    else if (x < 0 && y > 0) res = "eto chetvert' # 3";
-    else if (x < 0 && y < 0) res = "eto chetvert' # 4";
-    else res = "tochka nahodistsa na koordinatnoy osi";
-    return res;
-}
+// string CheckKoord2(int x, int y)
+// {
+//     string res;
+//     if (x > 0 && y > 0) res = "eto chetvert' # 1";
+//     else if (x > 0 && y < 0) res = "eto chetvert' # 2";
+//     else if (x < 0 && y > 0) res = "eto chetvert' # 3";
+//     else if (x < 0 && y < 0) res = "eto chetvert' # 4";
+//     else res = "tochka nahodistsa na koordinatnoy osi";
+//     return res;
+// }
 
-try
-{
-    Console.WriteLine("Vvedite koordinaty X");
-    int x = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Vvedite koordinaty Y");
-    int y = Convert.ToInt32(Console.ReadLine());
-    CheckKoord(x, y);
-    Console.WriteLine(CheckKoord2(x,y));
-}
-catch
-{
-    Console.WriteLine("Nyzno bilo vvodit' celie chisla");
-}
+// try
+// {
+//     Console.WriteLine("Vvedite koordinaty X");
+//     int x = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Vvedite koordinaty Y");
+//     int y = Convert.ToInt32(Console.ReadLine());
+//     CheckKoord(x, y);
+//     Console.WriteLine(CheckKoord2(x,y));
+// }
+// catch
+// {
+//     Console.WriteLine("Nyzno bilo vvodit' celie chisla");
+// }
+
+
+
+Console.Write("Введите кол-во чисел для вывода:");
+            int N = int.Parse(Console.ReadLine());
+ 
+            Console.WriteLine("Число" + "\t" + "Квадрат");
+            for(int i = 1; i <= N; i++)
+                Console.WriteLine(i + "\t" + i*i);
+ 
+            Console.ReadLine();
+  
 
