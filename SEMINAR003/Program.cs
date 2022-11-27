5c59714c46af540e1117b33886e909a2b03eefde
Kirill Denisevitch <denviko00@gmail.com>
2022-11-27 11:53:29 +0300

    zadacha1

--- SEMINAR003/Program.cs
+++ SEMINAR003/Program.cs
@@ -36,14 +36,52 @@
 // }
 
 
-
-Console.Write("Введите кол-во чисел для вывода:");
-            int N = int.Parse(Console.ReadLine());
+// Zadacha2
+// Console.Write("Введите кол-во чисел для вывода:");
+//             int N = int.Parse(Console.ReadLine());
  
-            Console.WriteLine("Число" + "\t" + "Квадрат");
-            for(int i = 1; i <= N; i++)
-                Console.WriteLine(i + "\t" + i*i);
+//             Console.WriteLine("Число" + "\t" + "Квадрат");
+//             for(int i = 1; i <= N; i++)
+//                 Console.WriteLine(i + "\t" + i*i);
  
-            Console.ReadLine();
+//             Console.ReadLine();
   
 
+
+
+// Zadacha1
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+// l = √ (x 2 - x 1) 2+ (y 2 - y 1) 2
+
+double Rast(double  x1, double  x2, double y1, double y2)
+{
+    double distance = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2));
+    return distance;
+}
+
+try
+{
+    Console.Write("Введите координаты x1: ");
+    double x1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Введите координаты x2: ");
+    double x2 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Введите координаты y1: ");
+    double y1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Введите координаты y2: ");
+    double y2 = Convert.ToDouble(Console.ReadLine());
+
+    
+    Console.WriteLine(Rast(x1, x2, y1, y2));
+}
+
+catch
+{
+    Console.WriteLine("Проверьте правильность ввода данных");
+}
