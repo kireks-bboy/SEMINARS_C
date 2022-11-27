5ee3174fd291b69174186c5932cfb1f6a380ef89
Kirill Denisevitch <denviko00@gmail.com>
2022-11-27 10:28:42 +0300

    void2

--- SEMINAR003/Program.cs
+++ SEMINAR003/Program.cs
@@ -4,16 +4,23 @@
 
 void checkKoord(int x, int y)
 {
-if (x>0 && y>0) System.Console.WriteLine("Eto chetvert' #1");
-else if (x>0 && y<0) System.Console.WriteLine("Eto chetvert' #2");
-else if (x<0 && y>0) System.Console.WriteLine("Eto chetvert' #3");
-else if (x<0 && y<0) System.Console.WriteLine("Eto chetvert' #4");
-else System.Console.WriteLine("Tochka nahoditsya na koordinatnoi osi");
+  if (x > 0 && y > 0) System.Console.WriteLine("Eto chetvert' #1");
+  else if (x > 0 && y < 0) System.Console.WriteLine("Eto chetvert' #2");
+  else if (x < 0 && y > 0) System.Console.WriteLine("Eto chetvert' #3");
+  else if (x < 0 && y < 0) System.Console.WriteLine("Eto chetvert' #4");
+  else System.Console.WriteLine("Tochka nahoditsya na koordinatnoi osi");
 }
 
-System.Console.WriteLine("Vvedite koordinaty X");
-int x = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Vvedite koordinaty Y");
-int y = Convert.ToInt32(Console.ReadLine());
-checkKoord(x, y);
+try
+{
+  System.Console.WriteLine("Vvedite koordinaty X");
+  int x = Convert.ToInt32(Console.ReadLine());
+  System.Console.WriteLine("Vvedite koordinaty Y");
+  int y = Convert.ToInt32(Console.ReadLine());
+  checkKoord(x, y);
+}
+catch
+{
+  System.Console.WriteLine("Nado bilo vvodit' celie chisla!");
+}
 
