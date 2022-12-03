746747c99e6cb2f010dc4a65e40dc13e626065ec
Kirill Denisevitch <denviko00@gmail.com>
2022-12-03 16:41:24 +0300

    one

---
+++ SEMINAR004/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+int SumNumbers(int num)
+{
+  int sum = 0;
+  for (int i = 1; i <= num; i++)
+
+    sum += i; //sum=sum+i
+  return sum;
+}
+
+System.Console.WriteLine("Введите цлеое число: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine($"Сумма всех чисел от 1 до {num} равна {SumNumbers(num)}");
