aa24c726b066511e9aec34b57669643046805c17
Kirill Denisevitch <denviko00@gmail.com>
2022-12-03 17:37:41 +0300

    two

--- SEMINAR004/Program.cs
+++ SEMINAR004/Program.cs
@@ -1,14 +1,45 @@
-﻿
+﻿// ONE
 
-int SumNumbers(int num)
+// int SumNumbers(int num)
+// {
+//   int sum = 0;
+//   for (int i = 1; i <= num; i++)
+
+//     sum += i; //sum=sum+i
+//   return sum;
+// }
+
+// System.Console.WriteLine("Введите цлеое число: ");
+// int num = Convert.ToInt32(System.Console.ReadLine());
+// System.Console.WriteLine($"Сумма всех чисел от 1 до {num} равна {SumNumbers(num)}");
+
+
+
+// TWO
+// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+//89126 -> 5
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int X = Acount(num);
+Console.WriteLine($"Количестов цифр в числе равно {X}");
+
+int Acount(int num)
 {
-  int sum = 0;
-  for (int i = 1; i <= num; i++)
+int count = 0;
 
-    sum += i; //sum=sum+i
-  return sum;
+while (num != 0)
+{
+num = num / 10;
+count = count + 1;
+}
+return count;
 }
 
-System.Console.WriteLine("Введите цлеое число: ");
-int num = Convert.ToInt32(System.Console.ReadLine());
-System.Console.WriteLine($"Сумма всех чисел от 1 до {num} равна {SumNumbers(num)}");
+
+
+
+
+
+
